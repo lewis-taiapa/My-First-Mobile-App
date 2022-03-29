33a1ea5f63fb1c303016e7d0811b5d55ff9e70b1
Lewis-Taiapa <lewis.taiapa@gmail.com>
2022-03-29 22:38:18 +1300

    Created new project folder for Zelda Mobile App

---
+++ Zelda-Minesweeper/App.js
@@ -0,0 +1,180 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const minesweeper = require('./lib/minesweeper')
+const lib = require('./lib/lib')
+const tests = require('./lib/tests')
+
+export default function App() {
+
+  const [difficulty, setDifficulty] = useState('hard')
+
+  minesweeper.test(difficulty)
+  
+  // (setDifficulty(this.value); playAudio('restart'); removeWinPic();)}
+
+
+  return (
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+
+    <View> 
+      {/* id="menuheader"> */}
+      <Picker 
+        // id="diffSelect" 
+        selectedValue = {difficulty}
+        onValueChange = {(itemValue, itemIndex) => setDifficulty(itemValue)}
+        >
+          <Picker.Item label="Easy" value="easy" />
+          <Picker.Item label="Medium" value="medium" />
+          <Picker.Item label="Hard" value="hard" />
+          <Picker.Item label="Extreme" value="Extreme" />
+      </Picker>
+    </View>
+    // {/* <View id="message"></View>
+    // <Button id="btn" onclick="setDifficulty(diffSelect.value); playAudio('restart'); removeWinPic();">Reset</Button>
+    // <View id="notes"></View>
+    // </View> */}
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },})
+  
+//   body: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     flexDirection: 'column',
+//     backgroundImage: url("images/link1.jpeg");
+//     background-repeat: no-repeat;
+//     background-size: cover;
+//     fontFamily: 'sans-serif'
+//   }
+  
+//   #message, #notes {
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//   }
+  
+//   #message {
+//     float:left;
+//     font-size: 1.5rem;
+//     z-index: 1;
+//     margin-right: 10px;
+//     margin-left: 10px;
+//   }
+  
+//   .board {
+//     display: flex;
+//     flex-wrap: wrap;
+//     z-index: 1;
+//   }
+  
+//   .board div {
+//     color: green;
+//     font-size: 2rem;
+//     width: 75px;
+//     height: 75px;
+//     border: 1px #ccc dotted;
+//     border-radius: 10%;
+//     background-color: white;
+//     margin: 3px;
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//   }
+  
+//   .board div:hover {
+//     background-color: white;
+//   }
+  
+//   div.mine, div.mine:hover {
+//     background: no-repeat center/60% url("images/bomb.svg") crimson;
+//   }
+  
+//   div.hidden {
+//     background: #eee;
+//   }
+  
+//   div.hidden:hover {
+//     background: #ccc;
+//   }
+  
+//   div.marked, div.marked:hover {
+//     background: no-repeat center/60% url("images/mark.svg") deepskyblue;
+//   }
+  
+//   #btn:hover {
+//     background-color: #059862 !important;
+//     color: white!important;
+//   }
+//   #btn {
+//     float:left;
+//     background-color: #04AA6D;
+//     color: white;
+//     font-family: 'Source Sans Pro', sans-serif;
+//     font-size: 18px;
+//     padding: 6px 25px;
+//     margin-top: 4px;
+//     border-radius: 5px;
+//     word-spacing: 10px;
+//     border: none;
+//     z-index: 0;
+//     position: relative;
+//     top: 22%;
+//   }
+  
+//   /* Add winner css to show picture when user wins the game */
+  
+//   #wrapper {
+//     position: relative;
+//   }
+  
+//   .board.winner {
+//     opacity: 0;
+//     transition-duration: 1s;
+//   }
+  
+//   #winpictureDisplay.winner {
+//     opacity: 100;
+//     transition-duration: 4s;
+//   }
+  
+//   #winpictureDisplay {
+//     position: absolute;
+//     height: 100%;
+//     background-image: url('images/linkwin.jpg');
+//     background-repeat: no-repeat;
+//     background-position: top;
+//     z-index: 0;
+//     opacity: 0;
+//     background-size: contain;
+//     border-radius: 5px;
+//   }
+  
+//   /* Menu Header CSS */
+  
+//   #menuheader {
+//     position: relative;
+//     overflow: hidden;
+//   }
+  
+//   #myForm {
+//     float:left;
+//     position: relative;
+//     top: 40%;
+//     left: 0px;
+//     z-index: 2;
+//   }
+
+// });
---
+++ Zelda-Minesweeper/lib/minesweeper.js
@@ -0,0 +1,145 @@
+document.addEventListener('DOMContentLoaded', startGame)
+var difficulty = 'medium'
+
+// Define your `board` object here!
+var board = {cells: []}
+
+function test(word) {
+  console.log(word)
+}
+
+function createBoard(diff) {
+  switch (diff){
+    case 'easy': populateBoard(3,9); break;
+    case 'medium': populateBoard(4,16); break;
+    case 'hard': populateBoard(5,25); break;
+    case 'extreme': populateBoard(6,36); break;
+  }
+  setMines()
+  console.log(board.cells)
+}
+
+function populateBoard(rows,cells) {
+    for (let i = 0, colCount = 0, rowCount = 0; i < cells; i++, rowCount++, colCount = Math.floor(i/rows)){
+      board.cells.push({row: rowCount, col: colCount, isMine:true, hidden: true})
+      if(rowCount == rows - 1){
+        rowCount = -1
+      }
+    }
+}
+
+function setMines(){
+  for (element of board.cells){
+    element.isMine = Math.random() < .3
+  }
+}
+
+
+function startGame () {
+  // Don't remove this function call: it makes the game work!
+  createBoard(difficulty)
+  for(cell of board.cells){
+    cell.surroundingMines = countSurroundingMines(cell)
+  }
+  lib.initBoard()
+  addHoverAudio()
+  document.addEventListener('click', checkForWin)
+  document.addEventListener('contextmenu', checkForWin)
+}
+
+// Define this function to look for a win condition:
+//
+// 1. Are all of the cells that are NOT mines visible?
+// 2. Are all of the mines marked?
+function checkForWin () {
+
+  let win
+
+  for (cell of board.cells) {
+    if ((cell.isMine == false && cell.hidden == false) || (cell.isMine == true && cell.isMarked == true)){
+            win = true
+        }
+        else {
+            return
+        }
+      }
+    
+  // You can use this function call to declare a winner (once you've
+  // detected that they've won, that is!)
+
+  if(win == true){
+    lib.displayMessage('You win!')
+    playAudio('win')
+    document.getElementsByClassName('board')[0].setAttribute('class', 'board winner')
+    document.getElementById('winpictureDisplay').setAttribute('class', 'winner')
+    document.getElementById('winpictureDisplay').setAttribute('style', document.getElementsByClassName('board')[0].getAttribute('style'))
+  }
+  
+}
+
+// Define this function to count the number of mines around the cell
+// (there could be as many as 8). You don't have to get the surrounding
+// cells yourself! Just use `lib.getSurroundingCells`: 
+//
+//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+//
+// It will return cell objects in an array. You should loop through 
+// them, counting the number of times `cell.isMine` is true.
+function countSurroundingMines (cell) {
+  let surroundingCellsArray = lib.getSurroundingCells(cell.row, cell.col)
+  let count = 0
+    for (element of surroundingCellsArray) {
+      if (element.isMine == true){
+        ++count
+      }
+    }
+  return count
+}
+
+// This is the Function used to set the value of the Boards Difficulty
+
+function setDifficulty (val) {
+  difficulty = val
+  console.log(difficulty)
+  board.cells = []
+  emptyDOMGrid()
+  startGame()
+}
+
+function emptyDOMGrid (){
+  var boardNode = document.getElementsByClassName('board')[0]
+  while (boardNode.firstChild) {
+    boardNode.removeChild(boardNode.firstChild);
+    }
+}
+
+// function to remove the wining picture
+
+function removeWinPic () {
+  document.getElementsByClassName('board')[0].setAttribute('class', 'board')
+  document.getElementById('winpictureDisplay').setAttribute('class', '')
+  document.getElementById('winpictureDisplay').setAttribute('style', '')
+}
+
+// This function adds the Audio Classes to the cells
+
+function addHoverAudio(){
+  var boardNode = document.getElementsByClassName('board')[0]
+  console.log(boardNode)
+    for(let i = 0; i< boardNode.childNodes.length;i++){
+      boardNode.childNodes[i].setAttribute("onmouseenter", `playAudio("hover${i}")`)
+      boardNode.insertAdjacentHTML("afterend", `<audio id='hover${i}' preload='auto'><source src='audio/hover2.mp3'><source src='audio/hover2.ogg'>Your browser isn't invited for super fun time.</audio>`)
+      boardNode.childNodes[i].setAttribute("oncontextmenu", `playAudio("mark")`)
+        if(boardNode.childNodes[i].getAttribute('class').includes('mine') ? true : false){
+          boardNode.childNodes[i].setAttribute("onclick", `playAudio("loose")`)
+        }
+        else{
+          boardNode.childNodes[i].setAttribute("onclick", `playAudio("open")`)
+        } 
+    }
+}
+
+function playAudio(str) { 
+  var x = document.getElementById(str); 
+  x.play(); 
+}
